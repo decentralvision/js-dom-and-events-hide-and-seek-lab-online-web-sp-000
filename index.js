ce7eb3e94b82a1d11bed497a68d91d71df861386
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-11 15:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ const increaseRankBy = (n) => {
 }
 
 const deepestChild = () => {
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
 
+  while ( nextNode ) {
+    node = nextNode
+    nextNode = node.children[ 0 ]
+  }
+
+  return node
 }
